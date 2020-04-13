5177d3284a02731427e0045386e3aed464dad085
Viet Anh Le <mc.vietanh@gmail.com>
2020-04-13 15:37:51 +0700

    done convert

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
-import CharPicker from './components/CharPicker';
-import Character from './components/Character';
+import CharPicker from "./components/CharPicker";
+import Character from "./components/Character";
 
-class App extends Component {
-  state = {
-    selectedCharacter: 1,
-    side: 'light',
-    destroyed: false
+const App = (props) => {
+  // const [state, setState] = useState({
+  //   selectedCharacter: 1,
+  //   side: "light",
+  //   destroyed: false,
+  // });
+
+  const [destroyed, setDestroyed] = useState(false);
+
+  const [selectedCharacter, setSelectedCharacter] = useState(1);
+
+  const [chosenSide, setChosenSide] = useState("light");
+
+  const sideHandler = (side) => {
+    setChosenSide(side);
   };
 
-  sideHandler = side => {
-    this.setState({ side: side });
-  };
-
-  charSelectHandler = event => {
+  const charSelectHandler = (event) => {
     const charId = event.target.value;
-    this.setState({ selectedCharacter: charId });
+    setSelectedCharacter(charId);
   };
 
-  destructionHandler = () => {
-    this.setState({ destroyed: true });
+  const destructionHandler = (destroyed) => {
+    setDestroyed(true);
   };
 
-  render() {
-    let content = (
-      <React.Fragment>
-        <CharPicker
-          side={this.state.side}
-          selectedChar={this.state.selectedCharacter}
-          onCharSelect={this.charSelectHandler}
-        />
-        <Character selectedChar={this.state.selectedCharacter} />
-        <button onClick={this.sideHandler.bind(this, 'light')}>
-          Light Side
-        </button>
-        <button onClick={this.sideHandler.bind(this, 'dark')}>Dark Side</button>
-        {this.state.side === 'dark' && (
-          <button onClick={this.destructionHandler}>DESTROY!</button>
-        )}
-      </React.Fragment>
-    );
-
-    if (this.state.destroyed) {
-      content = <h1>Total destruction!</h1>;
-    }
-    return content;
+  let content = (
+    <React.Fragment>
+      <CharPicker
+        side={chosenSide}
+        selectedChar={selectedCharacter}
+        onCharSelect={charSelectHandler}
+      />
+      <Character selectedChar={selectedCharacter} />
+      <button onClick={sideHandler.bind(this, "light")}>Light Side</button>
+      <button onClick={sideHandler.bind(this, "dark")}>Dark Side</button>
+      {chosenSide === "dark" && (
+        <button onClick={() => destructionHandler(destroyed)}>DESTROY!</button>
+      )}
+    </React.Fragment>
+  );
+  if (destroyed === true) {
+    content = <h1>Total destruction!</h1>;
   }
-}
+  return content;
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,86 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Summary from './Summary';
+import Summary from "./Summary";
 
-class Character extends Component {
-  state = { loadedCharacter: {}, isLoading: false };
+const Character = (props) => {
+  const [loadedCharacter, setLoadedCharacter] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate');
-    return (
-      nextProps.selectedChar !== this.props.selectedChar ||
-      nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
-      nextState.isLoading !== this.state.isLoading
-    );
-  }
-
-  componentDidUpdate(prevProps) {
-    console.log('Component did update');
-    if (prevProps.selectedChar !== this.props.selectedChar) {
-      this.fetchData();
-    }
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData = () => {
+  const fetchData = () => {
     console.log(
-      'Sending Http request for new character with id ' +
-        this.props.selectedChar
+      "Sending Http request for new character with id " + props.selectedChar
     );
-    this.setState({ isLoading: true });
-    fetch('http://localhost:4000/people/' + this.props.selectedChar)
-      .then(response => {
+    setIsLoading(true);
+    fetch("http://localhost:4000/people/" + props.selectedChar)
+      .then((response) => {
         if (!response.ok) {
-          throw new Error('Could not fetch person!');
+          throw new Error("Could not fetch person!");
         }
         return response.json();
       })
-      .then(charData => {
+      .then((charData) => {
         console.log(charData);
         const loadedCharacter = {
-          id: this.props.selectedChar,
+          id: props.selectedChar,
           name: charData.name,
           height: charData.height,
           colors: {
             hair: charData.hair_color,
-            skin: charData.skin_color
+            skin: charData.skin_color,
           },
           gender: charData.gender,
-          movieCount: charData.films.length
+          movieCount: charData.films.length,
         };
-        this.setState({ loadedCharacter: loadedCharacter, isLoading: false });
+        setLoadedCharacter(loadedCharacter);
+        setIsLoading(false);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
+        setIsLoading(false);
       });
   };
 
-  componentWillUnmount() {
-    console.log('Too soon...');
-  }
+  useEffect(() => {
+    fetchData();
+    return () => {
+      console.log('Cleaning up....')
+    };
+  }, [props.selectedChar]);
 
-  render() {
-    let content = <p>Loading Character...</p>;
+  let content = <p>Loading Character...</p>;
 
-    if (!this.state.isLoading && this.state.loadedCharacter.id) {
-      content = (
-        <Summary
-          name={this.state.loadedCharacter.name}
-          gender={this.state.loadedCharacter.gender}
-          height={this.state.loadedCharacter.height}
-          hairColor={this.state.loadedCharacter.colors.hair}
-          skinColor={this.state.loadedCharacter.colors.skin}
-          movieCount={this.state.loadedCharacter.movieCount}
-        />
-      );
-    } else if (!this.state.isLoading && !this.state.loadedCharacter.id) {
-      content = <p>Failed to fetch character.</p>;
-    }
-    return content;
+  if (!isLoading && loadedCharacter.id) {
+    content = (
+      <Summary
+        name={loadedCharacter.name}
+        gender={loadedCharacter.gender}
+        height={loadedCharacter.height}
+        hairColor={loadedCharacter.colors.hair}
+        skinColor={loadedCharacter.colors.skin}
+        movieCount={loadedCharacter.movieCount}
+      />
+    );
+  } else if (!isLoading && !loadedCharacter.id) {
+    content = <p>Failed to fetch character.</p>;
   }
-}
+  return content;
+};
 
 export default Character;
